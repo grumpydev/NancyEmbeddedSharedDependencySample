e3fed0e1e2056daf7a487d1b7b1063012f6a3576
Steven Robbins <ste.robbins@gmail.com>
2014-02-08 15:14:51 +0000

    added comment

--- NancyEmbeddedSharedDependencySample/Nancy/TestModule.cs
+++ NancyEmbeddedSharedDependencySample/Nancy/TestModule.cs
@@ -6,6 +6,11 @@
 
     public class TestModule : NancyModule
     {
+        /// <summary>
+        /// Test module
+        /// </summary>
+        /// <param name="dependency">The up front created dependency</param>
+        /// <param name="delayedDependency">The func creates a "lazy factory" for dependencies that may not exist</param>
         public TestModule(IUpFrontDependency dependency, Func<IDelayedDependency> delayedDependency)
         {
             Get["/dependency"] = _ => dependency.Text;
